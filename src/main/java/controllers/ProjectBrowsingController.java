ca7ee37baac1889386e738abea8dce17eff9aaea
Domn Werner <wern0096@vandals.uidaho.edu>
2016-04-04 13:30:14 -0700

    Added ProjectBrowsingController class and backButton in ProjectBrowsing.fxml now brings the user back to the home screen.

---
+++ src/main/java/controllers/ProjectBrowsingController.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by Domn on 4/4/2016.
+ */
+public class ProjectBrowsingController
+{
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private void onBackButtonClick(ActionEvent event)
+    {
+        Stage stage = null;
+        Parent root = null;
+        if (event.getSource() == backButton)
+        {
+            FXMLLoader loader = new FXMLLoader();
+            stage = (Stage) backButton.getScene().getWindow();
+            try
+            {
+                root = loader.load(getClass().getResource("/fxml/Home.fxml"));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        if (root != null)
+        {
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        else
+        {
+            System.out.println("Null scene.");
+        }
+    }
+}
