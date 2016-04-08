7d2d73a5aeb921e0ce263d12d50c0591916f7bda
Brandon Ratcliff <nondon4@gmail.com>
2016-04-07 19:18:11 -0700

    Fixed NewProjectController.java. I messed it up when I merged in the Ebean-ORM branch

--- src/main/java/squire/controllers/NewProjectController.java
+++ src/main/java/squire/controllers/NewProjectController.java
@@ -2,15 +2,22 @@ package squire.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by MattDaniel on 3/31/16.
  */
-
 public class NewProjectController
 {
     @FXML private Button nextButton;
@@ -18,11 +25,29 @@ public class NewProjectController
     @FXML private Button finishButton;
     @FXML private Button cancelButton1;
     @FXML private Button cancelButton2;
+    @FXML private Button importFilesButton;
     @FXML public TabPane tabPane;
     @FXML public Tab projectDetailsTab;
     @FXML public Tab projectSettingsTab;
+    @FXML Parent root;
 
-    @FXML private void nextButtonClicked(ActionEvent event)
+
+    @FXML
+    private void importFilesButtonClicked(ActionEvent event)
+    {
+        Stage stage = null;
+        stage = (Stage) importFilesButton.getScene().getWindow();
+        //System.out.println(event.getSource());
+        if (event.getSource() == importFilesButton) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Import File(s)");
+           File selectedFile = fileChooser.showOpenDialog(stage);
+            System.out.println(selectedFile.getName());
+        }
+    }
+
+    @FXML
+    private void nextButtonClicked(ActionEvent event)
     {
         //System.out.println(event.getSource());
         if (event.getSource() == nextButton) {
@@ -30,13 +55,14 @@ public class NewProjectController
         }
     }
 
+
+
     @FXML private void backButtonClicked(ActionEvent event)
     {
        // System.out.println(event.getSource());
         if (event.getSource() == backButton)
         {
-            if (event.getSource() == backButton)
-            {
+            if (event.getSource() == backButton) {
                 tabPane.getSelectionModel().select(projectDetailsTab);
             }
         }
@@ -44,36 +70,65 @@ public class NewProjectController
 
     @FXML private void finishButtonClicked(ActionEvent event)
     {
+        Stage stage = null;
+        Parent root = null;
         if (event.getSource() == finishButton)
         {
             FXMLLoader loader = new FXMLLoader();
-            Stage stage = (Stage) finishButton.getScene().getWindow();
+            stage = (Stage) finishButton.getScene().getWindow();
             stage.setResizable(true);
             try
             {
-                Parent root = loader.load(getClass().getResource("/fxml/Editor.fxml"));
+                root = loader.load(getClass().getResource("/fxml/Editor.fxml"));
             }
-            catch (Exception e)
+            catch (IOException e)
             {
                 e.printStackTrace();
             }
         }
+
+        if (root != null)
+        {
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        else
+        {
+            System.out.println("Null scene.");
+        }
+
     }
+
 
     @FXML private void cancelButtonClicked(ActionEvent event)
     {
+        Stage stage = null;
+        Parent root = null;
         if (event.getSource() == cancelButton1 || event.getSource() == cancelButton2)
         {
             FXMLLoader loader = new FXMLLoader();
-            Stage stage = (Stage) cancelButton1.getScene().getWindow();
+            stage = (Stage) cancelButton1.getScene().getWindow();
             try
             {
-                Parent root = loader.load(getClass().getResource("/fxml/Home.fxml"));
+                root = loader.load(getClass().getResource("/fxml/Home.fxml"));
             }
-            catch (Exception e)
+            catch (IOException e)
             {
                 e.printStackTrace();
             }
         }
+
+        if (root != null)
+        {
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        else
+        {
+            System.out.println("Null scene.");
+        }
+
     }
 }
