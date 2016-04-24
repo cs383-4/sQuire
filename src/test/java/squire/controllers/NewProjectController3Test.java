dd84efa35d0b56378cb73294b766071408661705
dani2918 <dani2918@vandals.uidaho.edu>
2016-04-24 13:16:37 -0700

    Set up new project controller test shell
    mostly committing this because my computer just restarted…

---
+++ src/test/java/squire/controllers/NewProjectController3Test.java
@@ -0,0 +1,33 @@
+package squire.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MattDaniel on 4/24/16.
+ */
+public class NewProjectController3Test
+{
+    private  NewProjectController3 npc;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        npc = new NewProjectController3();
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testInitialize() throws Exception
+    {
+
+    }
+}
