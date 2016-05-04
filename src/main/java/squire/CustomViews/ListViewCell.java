079f238bf28b43dfed0aa3b27b1a34c60508e329
Domn Werner <domn.werner@gmail.com>
2016-05-03 18:26:47 -0700

    Created ProjectData class for storing project info for ListView Items.

--- src/main/java/squire/CustomViews/ListViewCell.java
+++ src/main/java/squire/CustomViews/ListViewCell.java
@@ -5,16 +5,16 @@ import javafx.scene.control.ListCell;
 /**
  * Created by Domn Werner on 5/2/2016.
  */
-public class ListViewCell extends ListCell<String>
+public class ListViewCell extends ListCell<ProjectData>
 {
     @Override
-    public void updateItem(String string, boolean empty)
+    public void updateItem(ProjectData data, boolean empty)
     {
-        super.updateItem(string, empty);
-        if (string != null)
+        super.updateItem(data, empty);
+        if (data != null)
         {
             ProjectListViewItem item = new ProjectListViewItem();
-            item.setInfo(string, string);
+            item.setInfo(data);
             setGraphic(item.getMainHBox());
         }
     }
