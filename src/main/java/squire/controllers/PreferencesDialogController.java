e2d11d01108305f6bc6cb2c006e9845f4d6908a9
OrganicMechanic <doum6708@vandals.uidaho.edu>
2016-05-03 21:37:43 -0700

    Properly displays email on Change Info Screen.

--- src/main/java/squire/controllers/PreferencesDialogController.java
+++ src/main/java/squire/controllers/PreferencesDialogController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -25,12 +26,21 @@ public class PreferencesDialogController implements Initializable
     @FXML private TextField confirmEmailTextField;
     @FXML private Button closeButton;
     @FXML private Button submitButton;
+    @FXML private Label usernameLabel;
+    @FXML private Label emailLabel;
 
     private Stage thisStage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        Response res = new Request("user/getUsernameFromSessionId").set("sessionID", Main.getSessionID()).send();
+        String username = (String)res.get("username");
+        usernameLabel.setText((String)res.get("username"));
+
+        Response emailres = new Request("user/getEmailFromUsername").set("sessionID", Main.getSessionID()).send();
+        String email = (String)emailres.get("email");
+        emailLabel.setText((String)emailres.get("email"));
 
     }
 
